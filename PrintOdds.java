499e9cfd009a06b9df6766d5d98aabbdd11f540e
Evan Merritt <109871289+MrMerrittx702@users.noreply.github.com>
2022-09-05 18:17:22 -0500

    Create PrintOdds.java

---
+++ PrintOdds.java
@@ -0,0 +1,42 @@
+/*
+Write a program that accepts an integer n and prints out the next n odd numbers starting at 0. If there are no odd numbers or n is more than 10 
+the program should print "no odds." 
+
+assume n >= 0;
+
+For example n = 3;
+
+output:
+1 3 5
+------
+n = 0;
+
+output:
+no odds.
+
+--------
+n = 10;
+
+output:
+no odds.
+
+---------------------------------------
+Don't use an actual number for your final program, use n in place of an actual number. 
+*/
+
+import java.util.Scanner;
+
+class PrintOdds{
+  public static void main(String[] args){
+    //-----------------------------------------
+    Scanner inputObj = new Scanner(System.in);
+    int n = inputObj.nextInt();
+    //ignore code above here-------------------
+    //Write your code below--------------------
+   
+    
+    
+    
+  }
+    
+}
